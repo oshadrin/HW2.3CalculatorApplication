ec672929107b8902da3e75bcd0cdd34e7b6ff295
olegshadrin <sholegemail@gmail.com>
2022-09-25 22:23:21 +0300

    first commit

---
+++ src/main/java/pro/sky/java/course2/calculatorapplication/controller/CalculatorController.java
@@ -0,0 +1,11 @@
+package pro.sky.java.course2.calculatorapplication.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+}
